d713be08b180ab0afb6659f2e88e174b0d022c1e
Juan Espinet <juan.espinet@gmail.com>
2017-01-24 14:44:58 -0800

    setup for anagram challenge

---
+++ HackerRank-Code-Challenges/src/juan/challenges/anagrams/Anagrams.java
@@ -0,0 +1,23 @@
+package juan.challenges.anagrams;
+
+import java.util.Scanner;
+
+/**
+ * My solution to a code challenge from hackerrank.com, 
+ * description at https://www.hackerrank.com/challenges/ctci-making-anagrams
+ * 
+ * @author juanespinet
+ *
+ */
+public class Anagrams {
+	public static int numberNeeded(String first, String second) {
+	      return -1;
+    }
+  
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        String b = in.next();
+        System.out.println(numberNeeded(a, b));
+    }
+}

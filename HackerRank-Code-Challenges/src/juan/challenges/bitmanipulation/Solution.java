7d96792c1c24dd203a2173112d665d77e54b7757
Juan Espinet <juan.espinet@gmail.com>
2017-02-04 04:01:24 -0800

    adds solution to connected cell grid problem, adds setup for bit manipulation problem

---
+++ HackerRank-Code-Challenges/src/juan/challenges/bitmanipulation/Solution.java
@@ -0,0 +1,13 @@
+package juan.challenges.bitmanipulation;
+
+/**
+ * Solution to a coding challenge from HackerRank.com
+ * Challenge description can be found at: https://www.hackerrank.com/challenges/ctci-lonely-integer
+ * 
+ * 
+ * @author juanespinet
+ *
+ */
+public class Solution {
+
+}

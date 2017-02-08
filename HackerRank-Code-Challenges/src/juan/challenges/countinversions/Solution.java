0277d44cb69f64401194a6588968703b39e161d6
Juan Espinet <juan.espinet@gmail.com>
2017-02-08 03:22:31 -0800

    merge function reworked to support counting inversions

--- HackerRank-Code-Challenges/src/juan/challenges/countinversions/Solution.java
+++ HackerRank-Code-Challenges/src/juan/challenges/countinversions/Solution.java
@@ -18,16 +18,85 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
+        
+        MergeShiftCounter counter = new MergeShiftCounter();
+        
         for(int a0 = 0; a0 < t; a0++){
             int n = in.nextInt();
             int arr[] = new int[n];
             for(int arr_i=0; arr_i < n; arr_i++){
                 arr[arr_i] = in.nextInt();
             }
+            System.out.println(counter.countShifts(arr));
         }
     }
-    
-    public static void mergeArraySections(int[] array, int leftStart, int rightEnd, int[] tempArray) {
+}
+
+/**
+ * Performs merge sort and counts the number of shifts required
+ * for that merge.
+ * 
+ * @author juanespinet
+ *
+ */
+class MergeShiftCounter {
+	long shifts = 0;
+	
+	/**
+	 * Performs a merge sort on an entire array
+	 * and returns the total number of shifts required
+	 * to perform this sort.
+	 * 
+	 * @param array
+	 */
+	public long countShifts(int[] array) {
+		// reset the shifts counter
+		shifts = 0;
+		
+		// run the sort, which internally increments this counter
+		this.mergeSort(array, 0, array.length - 1, null);
+		
+		return shifts;
+	}
+	
+	/**
+	 * Performs a merge sort on the given array segment and counts
+	 * the total number of shifts used in this sort.
+	 * 
+	 * @param array
+	 * @param leftStart
+	 * @param rightEnd
+	 * @param tempArray
+	 */
+	public void mergeSort(int[] array, int leftStart, int rightEnd, int[] tempArray) {
+		if (leftStart < rightEnd) {
+			// check to make sure we have been passed a temp array
+	    	// using one temp array for all recursive calls reduces memory complexity
+	    	int size = rightEnd - leftStart + 1;
+	    	if (tempArray == null) tempArray = new int[size];
+	    	
+	    	// find the midpoint
+	    	int mid = (leftStart + rightEnd) / 2;
+	    	
+	    	// divide into two halves and sort them
+	    	mergeSort(array, leftStart, mid, tempArray);
+	    	mergeSort(array, mid + 1, rightEnd, tempArray);
+	    	
+	    	// merge the sorted halves
+	    	mergeArraySections(array, leftStart, rightEnd, tempArray);
+		}
+	}
+	
+	/**
+	 * Merges two sections of an array and counts the number
+	 * of shifts that were required for each element.
+	 * 
+	 * @param array
+	 * @param leftStart
+	 * @param rightEnd
+	 * @param tempArray
+	 */
+	public void mergeArraySections(int[] array, int leftStart, int rightEnd, int[] tempArray) {
     	// check to make sure we have been passed a temp array
     	// using one temp array for all recursive calls reduces memory complexity
     	int size = rightEnd - leftStart + 1;
@@ -44,14 +113,15 @@ public class Solution {
     	// compare the two halves
     	while (leftPointer <= mid && rightPointer <= rightEnd) {
     		
-			if (array[leftPointer] < array[rightPointer]) {
+			if (array[leftPointer] <= array[rightPointer]) {
 				tempArray[tempIndex] = array[leftPointer];
 				leftPointer++;
 			} else {
 				tempArray[tempIndex] = array[rightPointer];
+				// increment the total count of left shifts performed
+				this.shifts += rightPointer - tempIndex;
 				rightPointer++;
 			}
-			
 			tempIndex++;
 		}
     	
--- HackerRank-Code-Challenges/src/juan/challenges/countinversions/SolutionTest.java
+++ HackerRank-Code-Challenges/src/juan/challenges/countinversions/SolutionTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 public class SolutionTest {
 	
+	MergeShiftCounter counter;
 	int[] array1;
 	int leftStart;
 	int rightEnd;
@@ -16,6 +17,7 @@ public class SolutionTest {
 
 	@Before
 	public void setUp() throws Exception {
+		counter = new MergeShiftCounter();
 		array1 = new int[]{1, 3, 7, 5, 2, 4, 6};
 		leftStart = 0;
 		rightEnd = array1.length - 1;
@@ -28,9 +30,32 @@ public class SolutionTest {
 		// are not already sorted
 		int[] expected = {1, 2, 3, 4, 6, 7, 5};
 		
-		Solution.mergeArraySections(array1, leftStart, rightEnd, null);
+		counter.mergeArraySections(array1, leftStart, rightEnd, null);
 		
 		assertArrayEquals(expected, array1);
 	}
+	
+	@Test
+	public void testMergeSort() {
+		int[] expected = {1, 2, 3, 4, 5, 6, 7};
+		
+		counter.mergeSort(array1, leftStart, rightEnd, null);
+		
+		assertArrayEquals(expected, array1);
+	}
+	
+	@Test
+	public void testCountShifts() {
+		int[] array2 = {1, 1, 1, 2, 2}; 
+		int[] array3 = {2, 1, 3, 1, 2}; 
 
+		long shifts = counter.countShifts(array1);
+		assertEquals(7, shifts);
+		
+		shifts = counter.countShifts(array2);
+		assertEquals(0, shifts);
+		
+		shifts = counter.countShifts(array3);
+		assertEquals(4, shifts);
+	}
 }

a1d758420201c91f35332ac56518212af67696b7
Juan Espinet <juan.espinet@gmail.com>
2017-02-08 02:06:52 -0800

    fixes issue created by commenting out code in unit test while debugging

--- HackerRank-Code-Challenges/src/juan/challenges/primality/SolutionTest.java
+++ HackerRank-Code-Challenges/src/juan/challenges/primality/SolutionTest.java
@@ -14,12 +14,12 @@ public class SolutionTest {
 	@Test
 	public void testIsPrime() {
 		assertTrue(Solution.isPrime(2));
-//		assertTrue(Solution.isPrime(5));
-//		assertFalse(Solution.isPrime(12));
-//		assertTrue(Solution.isPrime(17));
-//		assertFalse(Solution.isPrime(15));
-//		assertTrue(Solution.isPrime(23));
-//		assertFalse(Solution.isPrime(49));
-//		assertFalse(Solution.isPrime(979));
+		assertTrue(Solution.isPrime(5));
+		assertFalse(Solution.isPrime(12));
+		assertTrue(Solution.isPrime(17));
+		assertFalse(Solution.isPrime(15));
+		assertTrue(Solution.isPrime(23));
+		assertFalse(Solution.isPrime(49));
+		assertFalse(Solution.isPrime(979));
 	}
 }

5d1ab4beefe696c88e1b9b305ad4a99e0184ae34
Juan Espinet <juan.espinet@gmail.com>
2017-02-16 15:49:33 -0800

    initial commit for sorting comparator challenge

---
+++ HackerRank-Code-Challenges/src/juan/challenges/sortingcomparator/Solution.java
@@ -0,0 +1,13 @@
+package juan.challenges.sortingcomparator;
+
+/**
+ * Solution to hackerrank challenge Sorting: Comparator
+ * found at: https://www.hackerrank.com/challenges/ctci-comparator-sorting
+ * 
+ * 
+ * @author juanespinet
+ *
+ */
+public class Solution {
+
+}

0b42a507242e15b2a14d11af5b0b78a1c4f9f8a5
Juan Espinet <juan.espinet@gmail.com>
2017-02-23 23:17:46 -0800

    adds setup and test case setup for queues challenge

---
+++ HackerRank-Code-Challenges/src/juan/challenges/queuesstacks/MyQueue.java
@@ -0,0 +1,25 @@
+package juan.challenges.queuesstacks;
+
+import java.util.Stack;
+
+public class MyQueue<T> {
+	
+	protected Stack<Integer> inStack;
+	protected Stack<Integer> outStack;
+
+	public void enqueue(int nextInt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void dequeue() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int peek() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ HackerRank-Code-Challenges/src/juan/challenges/queuesstacks/MyQueueTest.java
@@ -0,0 +1,19 @@
+package juan.challenges.queuesstacks;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyQueueTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

57d6ea5df2b24bae4221d732137880fe66bb81cc
Juan Espinet <juan.espinet@gmail.com>
2017-03-15 19:01:18 -0700

    adds completed implementation challenge

--- HackerRank-Code-Challenges/src/juan/challenges/algorithms/appleandorange/Solution.java
+++ HackerRank-Code-Challenges/src/juan/challenges/algorithms/appleandorange/Solution.java
@@ -25,5 +25,66 @@ public class Solution {
         for(int orange_i=0; orange_i < n; orange_i++){
             orange[orange_i] = in.nextInt();
         }
+        
+        House myHouse = new House(s, t);
+        FruitTree appleTree = new FruitTree(a);
+        FruitTree orangeTree = new FruitTree(b);
+        
+        System.out.println(myHouse.dropAllFruit(apple, appleTree));
+        System.out.println(myHouse.dropAllFruit(orange, orangeTree));
     }
+}
+
+class House {
+	protected final int leftEdge;
+	protected final int rightEdge;
+	
+	public House(int left, int right) {
+		this.leftEdge = left;
+		this.rightEdge = right;
+	}
+	
+	public boolean isOnHouse(int coord) {
+		return (coord >= leftEdge) && (coord <= rightEdge);
+	}
+	
+	/**
+	 * Takes an array of relative positions and a FruitTree
+	 * object and returns how many of these fruit landed on
+	 * the roof of this house.
+	 * 
+	 * @param fruitRelativePositions
+	 * @param tree
+	 */
+	public int dropAllFruit(int[] fruitRelativePositions, FruitTree tree) {
+		int fruitOnRoof = 0;
+		
+		for (int i = 0; i < fruitRelativePositions.length; i++) {
+			if (this.isOnHouse(tree.dropFruit(fruitRelativePositions[i]))) {
+				fruitOnRoof++;
+			}
+		}
+		
+		return fruitOnRoof;
+	}
+}
+
+class FruitTree {
+	protected final int position;
+	
+	public FruitTree(int position) {
+		this.position = position;
+	}
+	
+	/**
+	 * Returns the absolute coordinate of a dropped
+	 * piece of fruit given a position relative to the
+	 * tree.
+	 * 
+	 * @param relativePosition
+	 * @return
+	 */
+	public int dropFruit(int relativePosition) {
+		return position + relativePosition;
+	}
 }
--- HackerRank-Code-Challenges/src/juan/challenges/algorithms/appleandorange/SolutionTest.java
+++ HackerRank-Code-Challenges/src/juan/challenges/algorithms/appleandorange/SolutionTest.java
@@ -6,14 +6,45 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class SolutionTest {
+	
+	public House testHouse;
+	public FruitTree appleTree;
+	public FruitTree orangeTree;
 
 	@Before
 	public void setUp() throws Exception {
+		testHouse = new House(-1, 5);
+		appleTree = new FruitTree(-4);
+		orangeTree = new FruitTree(12);
+	}
+	
+	@Test
+	public void testIsOnRoof() {
+		assertFalse(testHouse.isOnHouse(-2));
+		assertTrue(testHouse.isOnHouse(-1));
+		assertFalse(testHouse.isOnHouse(6));
+		assertTrue(testHouse.isOnHouse(3));
+		assertTrue(testHouse.isOnHouse(5));
+	}
+	
+	@Test
+	public void testDropAllFruit() {
+		int[] fruitPositions = {-1,1,-7,3,-8};
+	
+		assertEquals(1, testHouse.dropAllFruit(fruitPositions, appleTree));
+
+		assertEquals(2, testHouse.dropAllFruit(fruitPositions, orangeTree));
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDropFuit() {
+		assertEquals(-4, appleTree.dropFruit(0));
+		assertEquals(12, orangeTree.dropFruit(0));
+		
+		assertEquals(0, appleTree.dropFruit(4));
+		assertEquals(16, orangeTree.dropFruit(4));
+		
+		assertEquals(-10, appleTree.dropFruit(-6));
+		assertEquals(6, orangeTree.dropFruit(-6));
 	}
-
 }
